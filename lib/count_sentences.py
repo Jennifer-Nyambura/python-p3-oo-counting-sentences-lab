7f40c5fb88cd3844f4b9ad55aba9949530d8915a
Jennifer <jenninyambu@gmail.com>
2025-08-07 06:53:15 +0300

    Implement MyString and pass all tests

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = ""
+        self.value = value  # Triggers the setter
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        if isinstance(val, str):
+            self._value = val
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+    def is_question(self):
+        return self.value.endswith("?")
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+        import re
+        # Use regex to split by punctuation that could end a sentence
+        parts = re.split(r'[.!?]', self.value)
+        # Filter out empty strings and whitespace
+        sentences = [part for part in parts if part.strip()]
+        return len(sentences)
+
